83ed1ce929f0955a4a753b4dfd1496ffdbd74f3d
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-19 18:57:16 +0300

    tests

--- tests/test_simple_token.py
+++ tests/test_simple_token.py
@@ -1,10 +1,26 @@
+import pytest
+
 from ctok.simple_token import SimpleToken
 
 
-def test_created_token_is_going_on():
+def test_just_created_token_without_arguments():
     assert SimpleToken().cancelled == False
     assert SimpleToken().is_cancelled() == False
     assert SimpleToken().keep_on() == True
+
+
+@pytest.mark.parametrize('arguments,expected_cancelled_status', [
+    ([SimpleToken(), SimpleToken().cancel()], True),
+    ([SimpleToken()], False),
+    ([SimpleToken().cancel()], True),
+    ([SimpleToken(), SimpleToken()], False),
+    ([SimpleToken(), SimpleToken(), SimpleToken()], False),
+    ([SimpleToken(), SimpleToken(), SimpleToken(), SimpleToken()], False),
+])
+def test_just_created_token_with_arguments(arguments, expected_cancelled_status):
+    assert SimpleToken(*arguments).cancelled == expected_cancelled_status
+    assert SimpleToken(*arguments).is_cancelled() == expected_cancelled_status
+    assert SimpleToken(*arguments).keep_on() == (not expected_cancelled_status)
 
 
 def test_stopped_token_is_not_going_on():
