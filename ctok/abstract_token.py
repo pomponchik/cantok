0d51c8316dd193c9c49fd07606165b44eadfe357
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-19 18:56:57 +0300

    __repr__, __str__

--- ctok/abstract_token.py
+++ ctok/abstract_token.py
@@ -2,9 +2,19 @@ from abc import ABC, abstractmethod
 
 
 class AbstractToken(ABC):
-    def __init__(self, *tokens: 'AbstractToken'):
+    def __init__(self, *tokens: 'AbstractToken', cancelled=False):
         self.tokens = tokens
-        self._cancelled = False
+        self._cancelled = cancelled
+
+    def __repr__(self):
+        other_tokens = ', '.join([repr(x) for x in self.tokens])
+        if other_tokens:
+            other_tokens += ', '
+        return f'{type(self).__name__}({other_tokens}cancelled={self.cancelled})'
+
+    def __str__(self):
+        cancelled_flag = 'cancelled' if self.cancelled else 'not cancelled'
+        return f'<{type(self).__name__} ({cancelled_flag})>'
 
     @property
     def cancelled(self) -> bool:
@@ -25,8 +35,9 @@ class AbstractToken(ABC):
 
         return False
 
-    def cancel(self) -> None:
+    def cancel(self) -> 'AbstractToken':
         self._cancelled = True
+        return self
 
     @abstractmethod
     def superpower(self) -> bool:  # pragma: no cover
