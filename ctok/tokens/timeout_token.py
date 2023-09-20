8e0e0d2333d17d5cd41b2d6a4021efc7002b6584
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-20 22:23:49 +0300

    monotonic as an option

--- ctok/tokens/timeout_token.py
+++ ctok/tokens/timeout_token.py
@@ -1,4 +1,4 @@
-from time import monotonic_ns as current_time
+from time import monotonic_ns, perf_counter
 
 from typing import Union
 
@@ -7,14 +7,20 @@ from ctok import ConditionToken
 
 
 class TimeoutToken(ConditionToken):
-    def __init__(self, timeout: Union[int, float], *tokens: AbstractToken, cancelled: bool = False):
+    def __init__(self, timeout: Union[int, float], *tokens: AbstractToken, cancelled: bool = False, monotonic: bool = True):
         if timeout < 0:
             raise ValueError
 
-        timeout *= 1_000_000_000
+        if monotonic:
+            timeout *= 1_000_000_000
 
-        start_time = current_time()
-        def function() -> bool:
-            return current_time() >= (start_time + timeout)
+            start_time = monotonic_ns()
+            def function() -> bool:
+                return monotonic_ns() >= (start_time + timeout)
+
+        else:
+            start_time = perf_counter()
+            def function() -> bool:
+                return perf_counter() >= (start_time + timeout)
 
         super().__init__(function, *tokens, cancelled=cancelled)
