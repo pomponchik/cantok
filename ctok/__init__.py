5537e77b1be0c831c28baa6e8bfb62e5ad989b2e
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-21 00:37:10 +0300

    ruff

--- ctok/__init__.py
+++ ctok/__init__.py
@@ -1,5 +1,5 @@
-from ctok.tokens.simple_token import SimpleToken
-from ctok.tokens.condition_token import ConditionToken
+from ctok.tokens.simple_token import SimpleToken  # noqa: F401
+from ctok.tokens.condition_token import ConditionToken  # noqa: F401
 from ctok.tokens.timeout_token import TimeoutToken
 
 
