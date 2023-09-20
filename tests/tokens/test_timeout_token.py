738f279b653ccb9258f5ec79c6ade83e7f132014
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-20 22:31:22 +0300

    tests

--- tests/tokens/test_timeout_token.py
+++ tests/tokens/test_timeout_token.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 import pytest
 
 from ctok import TimeoutToken
@@ -29,3 +27,28 @@ def test_zero_timeout(zero_timeout, options):
     assert token.is_cancelled() == True
     assert token.keep_on() == False
     assert token.keep_on() == False
+
+
+@pytest.mark.parametrize(
+    'options',
+    [
+        {},
+        {'monotonic': True},
+        {'monotonic': False},
+    ],
+)
+@pytest.mark.parametrize(
+    'timeout',
+    [
+        -1,
+        -0.5,
+    ],
+)
+def test_less_than_zero_timeout(options, timeout):
+    with pytest.raises(ValueError):
+        TimeoutToken(timeout, **options)
+
+
+def test_raise_without_first_argument():
+    with pytest.raises(TypeError):
+        TimeoutToken()
