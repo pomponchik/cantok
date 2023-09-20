e4c48a5b83da07fda41f7fd7e0a34a9020252ac2
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-20 23:35:54 +0300

    default value if an exception raised

--- ctok/tokens/condition_token.py
+++ ctok/tokens/condition_token.py
@@ -5,10 +5,11 @@ from ctok.tokens.abstract_token import AbstractToken
 
 
 class ConditionToken(AbstractToken):
-    def __init__(self, function: Callable[[], bool], *tokens: AbstractToken, suppress_exceptions: bool = True, cancelled: bool = False):
+    def __init__(self, function: Callable[[], bool], *tokens: AbstractToken, suppress_exceptions: bool = True, cancelled: bool = False, default: bool = False):
         super().__init__(*tokens, cancelled=cancelled)
         self.function = function
         self.suppress_exceptions = suppress_exceptions
+        self.default = default
 
     def superpower(self) -> bool:
         if not self.suppress_exceptions:
@@ -18,7 +19,7 @@ class ConditionToken(AbstractToken):
             with suppress(Exception):
                 return self.run_function()
 
-        return False
+        return self.default
 
     def run_function(self) -> bool:
         result = self.function()
@@ -27,7 +28,7 @@ class ConditionToken(AbstractToken):
             if not self.suppress_exceptions:
                 raise TypeError(f'The condition function can only return a bool value. The passed function returned "{result}" ({type(result).__name__}).')
             else:
-                return False
+                return self.default
 
         return result
 
