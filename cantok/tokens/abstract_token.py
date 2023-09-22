296fe55f950294e26ab1f7720338623619f1602f
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-22 22:52:34 +0300

    typevar

--- cantok/tokens/abstract_token.py
+++ cantok/tokens/abstract_token.py
@@ -23,7 +23,7 @@ class AbstractToken(ABC):
         cancelled_flag = 'cancelled' if self.cancelled else 'not cancelled'
         return f'<{type(self).__name__} ({cancelled_flag})>'
 
-    def __add__(self, item: 'AbstractToken') -> TypeVar('SimpleToken'):
+    def __add__(self, item: 'AbstractToken') -> TypeVar['SimpleToken']:
         if not isinstance(item, AbstractToken):
             raise TypeError('Cancellation Token can only be combined with another Cancellation Token.')
 
