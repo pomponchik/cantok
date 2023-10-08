b5915c8552580605d29992d47b6d4c1b278d3b0e
Evgeniy Blinov <zheni-b@yandex.ru>
2023-10-09 01:27:04 +0300

    +1 test

--- tests/readme_examples/test_examples.py
+++ tests/readme_examples/test_examples.py
@@ -6,7 +6,7 @@ from cantok import ConditionToken, CounterToken, TimeoutToken
 
 counter = 0
 
-def test_cancel_simple_token():
+def test_cancel_simple_token_with_function_and_thread():
     def function(token):
         global counter
         while not token.cancelled:
@@ -16,5 +16,15 @@ def test_cancel_simple_token():
     thread = Thread(target=function, args=(token, ))
     thread.start()
     thread.join()
+
+    assert counter
+
+
+def test_cancel_simple_token_with_function_and_thread():
+    token = ConditionToken(lambda: randint(1, 100_000) == 1984) + CounterToken(400_000, direct=False) + TimeoutToken(1)
+    counter = 0
+
+    while token:
+      counter += 1
 
     assert counter
