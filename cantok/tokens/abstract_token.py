62266c8c2fe5b49f8faac468dfe1a37e8241b58c
Evgeniy Blinov <zheni-b@yandex.ru>
2024-01-31 16:02:07 +0300

    type hints

--- cantok/tokens/abstract_token.py
+++ cantok/tokens/abstract_token.py
@@ -18,27 +18,27 @@ class CancelCause(Enum):
     SUPERPOWER = 2
     NOT_CANCELLED = 3
 
-class WaitCoroutineWrapper(Coroutine):
+class WaitCoroutineWrapper(Coroutine):  # type: ignore[type-arg]
     def __init__(self, step: Union[int, float], token_for_wait: 'AbstractToken', token_for_check: 'AbstractToken') -> None:
         self.step = step
         self.token_for_wait = token_for_wait
         self.token_for_check = token_for_check
 
-        self.flags = {}
+        self.flags: Dict[str, bool] = {}
         self.coroutine = self.async_wait(step, self.flags, token_for_wait, token_for_check)
 
         weakref.finalize(self, self.sync_wait, step, self.flags, token_for_wait, token_for_check, self.coroutine)
 
-    def __await__(self):
+    def __await__(self) -> Any:
         return self.coroutine.__await__()
 
-    def send(self, value: Any):
+    def send(self, value: Any) -> Any:
         return self.coroutine.send(value)
 
-    def throw(self, exception_type: Any, value: Optional[Any] = None, traceback: Optional[TracebackType] = None):
+    def throw(self, exception_type: Any, value: Optional[Any] = None, traceback: Optional[TracebackType] = None) -> Any:
         return self.coroutine.throw(exception_type, value, traceback)
 
-    def close(self):
+    def close(self) -> None:
         self.coroutine.close()
 
     @staticmethod
@@ -52,7 +52,7 @@ class WaitCoroutineWrapper(Coroutine):
             token_for_check.check()
 
     @staticmethod
-    async def async_wait(step: Union[int, float], flags: Dict[str, bool], token_for_wait: 'AbstractToken', token_for_check: 'AbstractToken'):
+    async def async_wait(step: Union[int, float], flags: Dict[str, bool], token_for_wait: 'AbstractToken', token_for_check: 'AbstractToken') -> None:
         flags['used'] = True
 
         while token_for_wait:
