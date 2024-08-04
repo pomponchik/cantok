c2a8ae554cdae22db468a8400e25a88f73e62acb
Evgeniy Blinov <zheni-b@yandex.ru>
2024-08-04 23:42:48 +0300

    type hints

--- cantok/errors.py
+++ cantok/errors.py
@@ -2,7 +2,9 @@ from typing import Any
 
 
 class CancellationError(Exception):
-    def __init__(self, message: str, token: Any):
+    token: 'AbstractToken'  # type: ignore[name-defined]
+
+    def __init__(self, message: str, token: 'AbstractToken') -> None:  # type: ignore[name-defined]
         self.token = token
         super().__init__(message)
 
