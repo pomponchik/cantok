304597a3a53847007ba62d5c81a3e68f1e33a22a
Evgeniy Blinov <zheni-b@yandex.ru>
2024-08-08 19:03:58 +0300

    empty lines

--- cantok/types.py
+++ cantok/types.py
@@ -1,11 +1,11 @@
 import sys
 from collections.abc import Iterable
 
-
 if sys.version_info >= (3, 10):
     from typing import TypeAlias  # pragma: no cover
 else:
     from typing_extensions import TypeAlias  # pragma: no cover
+
 
 if sys.version_info >= (3, 9):
     IterableWithTokens: TypeAlias = Iterable['AbstractToken']  # type: ignore[name-defined, unused-ignore] # pragma: no cover
