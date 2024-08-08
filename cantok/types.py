82664e2922270a4afaba7414f2357f97b8c6fed8
Evgeniy Blinov <zheni-b@yandex.ru>
2024-08-08 16:01:12 +0300

    fucking stupid tuping

--- cantok/types.py
+++ cantok/types.py
@@ -2,12 +2,12 @@ import sys
 from collections.abc import Iterable
 
 
-if sys.version_info > (3, 9):
+if sys.version_info >= (3, 10):
     from typing import TypeAlias  # pragma: no cover
 else:
     from typing_extensions import TypeAlias  # pragma: no cover
 
-if sys.version_info > (3, 8):
+if sys.version_info >= (3, 9):
     IterableWithTokens: TypeAlias = Iterable['AbstractToken']  # type: ignore[name-defined, unused-ignore]
 else:
     IterableWithTokens = Iterable  # pragma: no cover
